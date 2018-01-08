1303fd6e75829c1939f9d51a195557eca4187604
Jacky Wang <wangrui821@hotmail.com>
2018-01-08 09:54:53 +0800

    Create common project, and define abstract base class for Domain Command and Event in it.

---
+++ occ-common/src/main/java/com/yonyou/occ/common/domain/AbstractDomainCommand.java
@@ -0,0 +1,10 @@
+package com.yonyou.occ.common.domain;
+
+/**
+ * Domain command abstract class.
+ *
+ * @author WangRui
+ * @date 2018-01-05 09:55:00
+ */
+public abstract class AbstractDomainCommand {
+}
---
+++ occ-common/src/main/java/com/yonyou/occ/common/domain/AbstractDomainEvent.java
@@ -0,0 +1,12 @@
+package com.yonyou.occ.common.domain;
+
+import java.io.Serializable;
+
+/**
+ * Domain event abstract class.
+ *
+ * @author WangRui
+ * @date 2018-01-05 09:55:00
+ */
+public abstract class AbstractDomainEvent implements Serializable {
+}
